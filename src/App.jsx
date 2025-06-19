941b99fe72afe05987b830946329d70261733c06
HikaruXA <quaintuser321@gmail.com>
2025-06-19 03:47:18 -0700

    Prettier installation test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
